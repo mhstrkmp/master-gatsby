a0f06d84724c839fbe8e3d38262196229ea8bb31
Matthias Heisterkamp <hello@mhstrkmp.dev>
2021-01-22 12:27:37 +0100

    Add custom preview and connect toppings

--- sanity/schemas/pizza.js
+++ sanity/schemas/pizza.js
@@ -37,5 +37,29 @@ export default {
       validation: (Rule) => Rule.min(1000).max(50000),
       // TODO: Add custom input component
     },
+    {
+      name: "toppings",
+      title: "Toppings",
+      type: "array",
+      of: [{ type: "reference", to: [{ type: "topping" }] }],
+    },
   ],
+  preview: {
+    select: {
+      title: "name",
+      media: "image",
+      topping0: "toppings.0.name",
+      topping1: "toppings.1.name",
+      topping2: "toppings.2.name",
+      topping3: "toppings.3.name",
+    },
+    prepare: ({ title, media, ...toppings }) => {
+      const tops = Object.values(toppings).filter(Boolean);
+      return {
+        title,
+        media,
+        subtitle: tops.join(", "),
+      };
+    },
+  },
 };
