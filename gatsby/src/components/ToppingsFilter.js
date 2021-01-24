a736517d1c78dbfcc5195839a085f6d2c7276bb6
Matthias Heisterkamp <hello@mhstrkmp.dev>
2021-01-24 16:33:43 +0100

    Add styling and refactor code

--- gatsby/src/components/ToppingsFilter.js
+++ gatsby/src/components/ToppingsFilter.js
@@ -49,15 +49,8 @@ function countPizzasInToppings(pizzas) {
 }
 
 export default function ToppingsFilter() {
-  const { toppings, pizzas } = useStaticQuery(graphql`
+  const { pizzas } = useStaticQuery(graphql`
     query {
-      toppings: allSanityTopping {
-        nodes {
-          name
-          id
-          vegetarian
-        }
-      }
       pizzas: allSanityPizza {
         nodes {
           toppings {
--- gatsby/src/templates/Pizza.js
+++ gatsby/src/templates/Pizza.js
@@ -1,8 +1,29 @@
-import { graphql } from "gatsby";
 import React from "react";
+import { graphql } from "gatsby";
+import Img from "gatsby-image";
+import styled from "styled-components";
 
-export default function SinglePizzaPage() {
-  return <p>Single Pizza</p>;
+const PizzaGrid = styled.div`
+  display: grid;
+  grid-gap: 2rem;
+  grid-template-columns: repeat(auto-fill, minmax(400px, 1fr));
+`;
+
+export default function SinglePizzaPage({ data }) {
+  const { pizza } = data;
+  return (
+    <PizzaGrid>
+      <Img fluid={pizza.image.asset.fluid} />
+      <div>
+        <h2 className="mark">{pizza.name}</h2>
+        <ul>
+          {pizza.toppings.map((topping) => (
+            <li key={topping.id}>{topping.name}</li>
+          ))}
+        </ul>
+      </div>
+    </PizzaGrid>
+  );
 }
 
 export const query = graphql`
