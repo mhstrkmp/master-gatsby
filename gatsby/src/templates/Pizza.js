fb89289e00ef2a7cdad7aaaab9ed8df97bdb1314
Matthias Heisterkamp <hello@mhstrkmp.dev>
2021-01-24 16:19:51 +0100

    Add dynamically pages with gatsby-node

---
+++ gatsby/src/templates/Pizza.js
@@ -0,0 +1,27 @@
+import { graphql } from "gatsby";
+import React from "react";
+
+export default function SinglePizzaPage() {
+  return <p>Single Pizza</p>;
+}
+
+export const query = graphql`
+  query($slug: String!) {
+    pizza: sanityPizza(slug: { current: { eq: $slug } }) {
+      name
+      id
+      image {
+        asset {
+          fluid(maxWidth: 800) {
+            ...GatsbySanityImageFluid
+          }
+        }
+      }
+      toppings {
+        name
+        id
+        vegetarian
+      }
+    }
+  }
+`;
